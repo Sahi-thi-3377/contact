ef55cef925fd9d6e953f785583354a80a4a04056
shouryaoza-vit <kingsodyssey4@gmal.com>
2026-08-06 10:59:44 +0530

    Initial commit: Add interest calcualtor

---
+++ interest.py
@@ -0,0 +1,8 @@
+def simple_interest(principal,rate,time):
+    return((principal*rate*time)/100)
+if __name__=="__main__":
+    p=1000
+    r=5
+    t=2
+    result=simple_interest(p,r,t)
+    print(result)
